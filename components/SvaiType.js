00b15e839a3315ec9a56401d24f021e9de25c2ff
Vladyslav Dashevskyi <vladislav.dashevskiy@gmail.com>
2024-06-06 14:48:42 +0300

    svai type second step

--- components/SvaiType.js
+++ components/SvaiType.js
@@ -11,24 +11,30 @@ const SvaiType = () => {
             <p className={styles.desc}>Технология строительства фундаментов на винтовых сваях не является новой. В большинстве случаев в советское время эта технология применялась для строительства мостов, переходов, причалов, маяков.</p>
         </div>
         <div className={styles.card}>
-            <h5 className={styles.title}>Фундамент на сваях</h5>
-            <p className={styles.desc}>Строительство это процесс, в основе которого лежит создание фундамента.</p>
+            <div>
+                <h5 className={styles.title}>Фундамент на сваях</h5>
+                <p className={styles.desc}>Строительство это процесс, в основе которого лежит создание фундамента.</p>
+            </div>
             <div className={styles.btn}>
-                <ArrowRightAltIcon sx={{ color: 'white' }}/>
+                <ArrowRightAltIcon sx={{ color: 'white', backgroundColor: '#00215B' }}/>
             </div>
         </div>
         <div className={styles.card}>
-            <h5 className={styles.title}>Установка свай</h5>
-            <p className={styles.desc}>Установка свай подразумевает процесс вкручивания свай на заданную глубину, для обеспечения ее несущей способности.</p>
+            <div>
+                <h5 className={styles.title}>Установка свай</h5>
+                <p className={styles.desc}>Установка свай подразумевает процесс вкручивания свай на заданную глубину, для обеспечения ее несущей способности.</p>
+            </div>
             <div className={styles.btn}>
-                <ArrowRightAltIcon sx={{ color: 'white' }}/>
+                <ArrowRightAltIcon sx={{ color: 'white', backgroundColor: '#80C658' }}/>
             </div>
         </div>
         <div className={styles.card}>
-            <h5 className={styles.title}>Литые наконечники</h5>
-            <p className={styles.desc}>Надежный, качественный фундамент – это основа и безопасность любого строения.</p>
+            <div>
+                <h5 className={styles.title}>Литые наконечники</h5>
+                <p className={styles.desc}>Надежный, качественный фундамент – это основа и безопасность любого строения.</p>
+            </div>
             <div className={styles.btn}>
-                <ArrowRightAltIcon sx={{ color: 'white' }}/>
+                <ArrowRightAltIcon sx={{ color: 'white', backgroundColor: '#00215B' }}/>
             </div>
         </div>
     </div>
