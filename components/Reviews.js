a160b4c432389cedc2edcd48707edb7c84eebc93
Vladyslav Dashevskyi <vladislav.dashevskiy@gmail.com>
2024-06-06 17:26:52 +0300

    gallery

--- components/Reviews.js
+++ components/Reviews.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 import styles from '../styles/Reviews.module.scss';
+import Image from 'next/image';
+import review from '../assets/review.png'
 
 const Reviews = () => {
 
@@ -7,12 +10,17 @@ const Reviews = () => {
     <div className={styles.container}>
         <div className={styles.title}>ОТЗЫВЫ</div>
         <h4 className={styles.sub}>Отзывы наших клиентов</h4>
-        <div>
-            <div></div>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
+        <div className={styles.reviews}>
+            <Image
+                src={review}
+                width={200}
+                alt="review"
+                loading="lazy"
+            />
+            <div className={styles.info}>
+                <div><FormatQuoteIcon sx={{ color: 'white', backgroundColor: '#80C658', fontSize: '35px' }}/></div>
+                <p className={styles.text}>Suspendisse tortor enim, varius et porttitor sit amet, posuere vitae massa. Proin ac quam eu erat semper sagittis in vitae elit. Nam neque erat, semper vel ultrices in, finibus eu magna. Pellentesque habitant morbi tristique</p>
+                <div className={styles.author}>Иван Иванов</div>
             </div>
         </div>
     </div>
