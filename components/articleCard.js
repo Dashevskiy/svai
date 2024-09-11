5d87e579f28a6227d8fcbace1f988970bbae90d6
Vladyslav Dashevskyi <vladislav.dashevskiy@gmail.com>
2024-09-11 14:04:12 +0300

    articles

--- components/articleCard.js
+++ components/articleCard.js
@@ -15,79 +15,68 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Link from 'next/link';
-// import styles from '../styles/ArticleCard.module.scss';
+import styles from '../styles/ArticleCard.module.scss';
 
 export default function ArticleCard({article}) {
-  const [expanded, setExpanded] = React.useState(false);
-console.log(article.img);
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            А
-          </Avatar>
-        }
-        title="Адміністратор Ваші палі"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image={article.img.src}
-        alt="зимове будівництво"
-      />
-      <CardContent>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            {article.title}
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            {article.subtitle}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing >
-        <Box width='100%' display="flex" justifyContent="space-between" alignItems="center">
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                <Link href="/">Детальніше</Link>
-            </Typography>
-        </Box>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+    <Box className={styles.container}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              А
+            </Avatar>
+          }
+          title="Адміністратор Ваші палі"
+          subheader={article.date}
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image={article.img.src}
+          alt="зимове будівництво"
+        />
         <CardContent>
-          <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
-          <Typography sx={{ marginBottom: 2 }}>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              {article.title}
           </Typography>
-          <Typography sx={{ marginBottom: 2 }}>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography sx={{ marginBottom: 2 }}>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+          <div className={styles.subtitle}>
+              {article.subtitle} 
+          </div>
+          <Link href="/"> Детальніше</Link>
         </CardContent>
-      </Collapse>
-    </Card>
+     
+        <Collapse timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
+            <Typography sx={{ marginBottom: 2 }}>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+              aside for 10 minutes.
+            </Typography>
+            <Typography sx={{ marginBottom: 2 }}>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+              medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+              occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+              large plate and set aside, leaving chicken and chorizo in the pan. Add
+              pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+              stirring often until thickened and fragrant, about 10 minutes. Add
+              saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography sx={{ marginBottom: 2 }}>
+              Add rice and stir very gently to distribute. Top with artichokes and
+              peppers, and cook without stirring, until most of the liquid is absorbed,
+              15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+              mussels, tucking them down into the rice, and cook again without
+              stirring, until mussels have opened and rice is just tender, 5 to 7
+              minutes more. (Discard any mussels that don&apos;t open.)
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Box>
   );
 }
--- pages/articles.js
+++ pages/articles.js
@@ -1,14 +1,17 @@
 import React from 'react';
-// import styles from '../styles/Articles.module.scss';
+import styles from '../styles/ArticlesPage.module.scss';
 import Layout from "../layouts/Layout";
 import ArticleCard from '../components/articleCard';
 import { articles } from "../helpers/articleData";
+import PageHeader from '../components/PageHeader';
+import image from '../assets/contacts.png';
 
 const Articles = () => {
 
   return (
     <Layout keywords={'Статті'} title={'Статті'}>
-        <div>
+         <PageHeader image={image} title={'Статті'}/>
+        <div className={styles.container}>
             {articles.map((article)=>{
                 return (
                     <ArticleCard article={article} key={article.id}/>
